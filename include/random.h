88d0b9c370d2c8e032210b49e64fbc409897ba1f
TimuriAlvarez <70894059+TimuriAlvarez@users.noreply.github.com>
2023-04-19 19:33:32 +0300

    Tricky PRNG docs added

--- include/random.h
+++ include/random.h
@@ -21,6 +21,7 @@ namespace Random::Uniform
 	 */
 	template<typename T>
 	inline T real(const T min, const T max) {
+		static_assert(std::is_floating_point_v<T>, "Uniform::real only works with floating point types");
 		std::random_device rdev;
 		return std::uniform_real_distribution<T>(std::min(min, max), std::max(min, max))(rdev);
 	}
@@ -35,30 +36,42 @@ namespace Random::Uniform
 	 */
 	template<typename T>
 	inline T integral(const T min, const T max) {
+		static_assert(std::is_integral_v<T>, "Uniform::integral only works with integral types");
 		std::random_device rdev;
 		return std::uniform_int_distribution<T>(std::min(min, max), std::max(min, max))(rdev);
 	}
 }
 
 namespace Random {
-	template<typename T, const size_t ALLOWED = 1lu>
+	template<typename T, const size_t BARRIER = 1lu>
 	class Tricky;
 }
 
-template<typename T, const size_t ALLOWED>
+/*!
+ * \brief Generates a random real number in the range [min, max]
+ * 
+ * \tparam T type of the number
+ * \tparam BARRIER number of times a number generation is forbidden after it was generated
+ * \example Tricky<int, 2> prng(0, 10); // Generates a random number in the range [0, 10], forbids a number to be generated 2 times after it was generated
+ * \example Tricky<int, 0> prng(0, 10); // Generates a random number in the range [0, 10], forbids a number to be generated 0 times after it was generated (equivalent to Uniform::integral)
+ * \param min minimum value of the range (inclusive)
+ * \param max maximum value of the range (inclusive)
+ * \return T random number in the range [min, max]
+ */
+template<typename T, const size_t BARRIER>
 class Random::Tricky {
 protected:
+	//! \brief Protection from invalid template parameters
 	static_assert(std::is_integral_v<T>, "Tricky PRNG only works with integral types");
-	static_assert(ALLOWED > 0lu, "ALLOWED must be greater than 0");
 
 public:
 	//! \brief Tricky PRNG constructor (min and max are inclusive)
 	Tricky(const T min, const T max): min(std::min(min, max)), max(std::max(min, max)) {
 		//! \note initialize chances vector
 		chances.resize(max - min + 1lu);
-		//! \note set all chances to ALLOWED + 1, required for prng to work properly and not stuck in an infinite loop
+		//! \note set all chances to BARRIER + 1, required for prng to work properly and not stuck in an infinite loop
 		for (auto& chance : chances)
-			chance = ALLOWED + 1lu;
+			chance = BARRIER + 1lu;
 	}
 
 	//! \brief Tricky PRNG operator, aka "generate a random number"
@@ -70,8 +83,8 @@ public:
 			size_t random = Uniform::integral<size_t>(0lu, sum - 1lu);
 			//! \note get the index of the number in chances vector that corresponds to the given random number
 			size_t index = index_of(random);
-			// If the number was chosen ALLOWED times ago, then skip it
-			if (chances[index] <= ALLOWED)
+			// If the number was chosen BARRIER times ago, then skip it
+			if (chances[index] <= BARRIER)
 				continue;
 			// Increment all chances
 			for (auto& chance : chances)
@@ -84,8 +97,9 @@ public:
 	}
 
 private:
-	//! \note internal variables
+	//! \note min and max values for generation
 	T min, max;
+	//! \note chances vector for generation
 	std::vector<size_t> chances;
 
 	//! \brief Returns the index of the number in chances vector that corresponds to the given random number
